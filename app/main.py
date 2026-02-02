0eb6fb04f515c490b190999e9af42099a446a65d
Emiliano Jankowski <ejankowski@gmail.com>
2026-02-02 12:59:08 +0100

    removing unused params

--- app/main.py
+++ app/main.py
@@ -69,13 +69,11 @@ def create_app(testing: bool = False, auth_middleware=None) -> FastAPI:
 
         app.add_middleware(
             UserOnboardingMiddleware,
-            identies_base_url=settings.identies_host,
             user_service_factory=user_service_factory,
         )
 
         app.add_middleware(
             AuthenticationMiddleware,
-            identies_base_url=settings.identies_host,
             skip_paths=SKIP_PATHS,
             user_service_factory=user_service_factory,
         )
