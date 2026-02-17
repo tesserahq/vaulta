1f5b064d739d6431ffd3e2134501a132678694d8
Emiliano Jankowski <ejankowski@gmail.com>
2026-02-17 10:20:48 +0100

    fixing missing import

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate, UserOnboard
 from datetime import datetime, timezone
+from sqlalchemy import or_
 
 from app.utils.db.filtering import apply_filters
 
